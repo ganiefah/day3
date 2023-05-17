27880bd98bb3634ae37955704a957ec2a18cbcad
ganiefah <ganiefahmajiet3@gmail.com>
2023-05-17 09:23:15 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,37 @@
+let firstName = 'Ganiefah'
+console.log (typeof firstName);
+//it will display as a string 
+//question 1
+
+firstName = 2 ;
+console.log(typeof firstName); 
+//it changes it from a string to a number 
+//question 2
+
+
+
+//question 3
+let statement = true ;
+console.log(typeof(statement));
+//will display a boolean
+
+let name = "John";
+console.log(typeof(name));
+//will display string 
+
+let number = 2;
+console.log(typeof(number));
+//will display number
+
+let lastName;
+console.log(typeof(lastName));
+//will display undefined
+
+
+let symbol = Symbol();
+console.log(typeof(symbol));
+// will display symbol
+
+let later = null;
+console.log(typeof(later));
+// will display null 
